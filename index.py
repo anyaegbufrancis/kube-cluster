10293c8cb08f73e5e691c3f54a78824727f2cb38
root <root@auto-machine.homelab.local>
2022-08-22 16:36:17 +0400

    Kubernetes Bootstrapping - completed play

--- index.py
+++ index.py
@@ -4,9 +4,9 @@ import os
 cmd = [
  "ansible-playbook template_play1.yaml -v", 
  "ansible-playbook template_play2.yaml -v", 
- "ansible-playbook other_plays.yaml -v"
+ "ansible-playbook template_play3.yaml -v",
+ "ansible-playbook template_play4.yaml -v",
+ "ansible-playbook template_play5.yaml -v"
   ]
 for command in cmd:
   os.system(command)
-
-print("Playbook run completed!!")
